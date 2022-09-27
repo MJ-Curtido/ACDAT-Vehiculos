03a162c4669c70bf7db16ee8e40b6a88e3191a98
MJ-Curtido <manu17pc@gmail.com>
2022-09-27 12:47:48 +0200

    V2-ClasesDAOsI

---
+++ src/main/java/dam/vehiculos/daos/DAOVehiculos.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dam.vehiculos.daos;
+
+import dam.vehiculos.clases.Vehiculo;
+import dam.vehiculos.interfaces.IDAOVehiculos;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Dam
+ */
+public class DAOVehiculos implements IDAOVehiculos {
+
+    private List<Vehiculo> falsaBD;
+    private static DAOVehiculos dao = null;
+
+    private DAOVehiculos() {
+        super();
+        this.falsaBD = new ArrayList<Vehiculo>();
+        falsaBD.add(new Vehiculo("Renault", "Zoe", "2345FDF"));
+        falsaBD.add(new Vehiculo("Renault", "Fluence", "0000FTL"));
+        falsaBD.add(new Vehiculo("Tesla", "3", "2422FHT"));
+        falsaBD.add(new Vehiculo("Tesla", "X", "1221FDF"));
+
+    }
+
+    @Override
+    public int insertarVehiculo(Vehiculo vehiculo) {
+        falsaBD.add(vehiculo);
+
+        return 1;
+    }
+
+    @Override
+    public int eliminarVehiculo(String matricula) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int eliminarVehiculos(List<Vehiculo> lstVehiculos) {
+
+        return 0;
+    }
+
+    @Override
+    public Vehiculo getVehiculo(String matricula) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Vehiculo> getVehiculos() {
+        // TODO Auto-generated method stub
+        return this.falsaBD;
+    }
+
+    public static DAOVehiculos getInstance() {
+        if (dao == null) {
+            dao = new DAOVehiculos();
+        }
+
+        return dao;
+    }
+
+    @Override
+    public int eliminarVehiculo(Vehiculo vehiculo) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
