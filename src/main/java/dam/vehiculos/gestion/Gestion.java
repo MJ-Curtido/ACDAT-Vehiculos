ec4ab7f3ccc36c48d2a227b0e190456d49006df9
MJ-Curtido <manu17pc@gmail.com>
2022-10-24 13:07:40 +0200

    Crear Gestion

---
+++ src/main/java/dam/vehiculos/gestion/Gestion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dam.vehiculos.gestion;
+
+import dam.vehiculos.clases.Vehiculo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dam
+ */
+public class Gestion {
+    private ArrayList<Vehiculo> listaVehiculo;
+}
